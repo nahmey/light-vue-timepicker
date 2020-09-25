3df83456b9567f04360f93cc65bb017719c25e2a
julien <julien.kennel@geauto.fr>
2020-09-25 13:47:25 +0200

    add lang + second + format 12/24 + update readme

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ Simple timepicker for VUE.JS made with Bootstrap
 - [Dependencies](#dependencies)
 - [Installation](#installation)
 - [Usage](#example-usage)
-- [Configuration](#configuration)
+- [Retrieve Data](#retrieve-data)
+- [Props](#props)
+- [Example](#example)
 
 ## Dependencies
 * Vue.js. Required.
@@ -33,24 +35,43 @@ Vue.component('light-vue-timepicker', LightVueTimepicker);
 
 ## Usage
 ```html
-<light-vue-timepicker
-v-model="time"
-:hourRange="['8-12', '14-19']"
-:classe="form-control col-5"
->
-</light-vue-timepicker>
+<light-vue-timepicker></light-vue-timepicker>
 ```
 
-## Configuration
+## Retrieve data
 
-
-```html
-comming soon
+```javascript
+variable = {
+	yourVmodel.hour,
+	yourVmodel.minute,
+	yourVmodel.second,
+	yourVmodel.a
+}
 ```
+
 ## Props
 
 | Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | Type | Description | Default
 | ----------------- | :--- | :--- | :--- |
-| `hourRange`      | `Array` | Range of hour which displayed (ex ['8-12', '14-19', '22']) | ['00-23'] |
-| `minuteRange`      | `Array` | Range of minute which displayed (ex ['0-30']) | ['00-59'] |
+| `hourRange`      | `Array` | Range of hours which displayed (ex ['8-12', '14-19', '22']) | ['00-23'] |
+| `minuteRange`      | `Array` | Range of minutes which displayed (ex ['0-30']) | ['00-59'] |
+| `secondRange`      | `Array` | Range of secondes which displayed (ex ['0-30']) | ['00-59'] |
 | `classe`      | `String` | class(boostrap or other) for input hour and minute | form-control col-5 |
+| `format`      | `String` | Format 12 or 24 | 24 |
+| `lang`      | `String` | lang fr or en  | null (display HH MM) |
+| `withHour`      | `Boolean` | Display input hour  | true |
+| `withMinute`      | `Boolean` | Display input minute  | true |
+| `withSecond`      | `Boolean` | Display input second  | false |
+
+
+## Example
+```html
+<light-vue-timepicker
+v-model="time"
+lang="en"
+:hourRange="['8-12', '14-19', '21']"
+:minuteRange="['30', '40', '55-57']"
+:withSecond="true"
+>
+</light-vue-timepicker>
+```
--- src/light-vue-timepicker.vue
+++ src/light-vue-timepicker.vue
@@ -1,17 +1,36 @@
 <template>
 	<div class="input-group">
-		<select :class="cl" @change="updateHour()" :value="initialValue" v-model="hour">
-			<option selected="selected" value="0">Heures</option>
-			<option v-for="hour in hours_array" :value="hour">
-				{{hour}}
-			</option>
-		</select>
-		<select :class="cl" @change="updateHour()" :value="initialValue" v-model="minute">
-			<option selected="selected" value="0">Min</option>
-			<option v-for="minute in minutes_array" :value="minute">
-				{{minute}}
-			</option>
-		</select>
+		<template v-if="withH">
+			<select :class="cl" @change="updateHour()" :value="initialValue" v-model="hour">
+				<option selected="selected" value="0">{{display_lang['hour']}}</option>
+				<option v-for="hour in hours_array" :value="hour">
+					{{hour}}
+				</option>
+			</select>
+		</template>
+		<template v-if="withM">
+			<select :class="cl" @change="updateHour()" :value="initialValue" v-model="minute">
+				<option selected="selected" value="0">{{display_lang['min']}}</option>
+				<option v-for="minute in minutes_array" :value="minute">
+					{{minute}}
+				</option>
+			</select>
+		</template>
+		<template v-if="withS">
+			<select :class="cl" @change="updateHour()" :value="initialValue" v-model="second">
+				<option selected="selected" value="0">{{display_lang['sec']}}</option>
+				<option v-for="second in seconds_array" :value="second">
+					{{second}}
+				</option>
+			</select>
+		</template>
+		<template v-if="format == '12'">
+			<select :class="cl" @change="updateHour()" :value="initialValue" v-model="a">
+				<option selected="selected" value="0">{{display_lang['a']}}</option>
+				<option value="am">AM</option>
+				<option value="pm">PM</option>
+			</select>
+		</template>
 		<div class="col-2 input-group-append" style="margin-left:-15px!important;">
 	        <span class="btn btn-primary"><i class="fas fa-clock"></i></span>
 	    </div>
@@ -22,25 +41,45 @@
 	export default {
 		name: 'LightVueTimepicker',
 		props :{
-			withoutHour: false,
-			withoutMinute: false,
+			withHour: Boolean,
+			withMinute: Boolean,
+			withSecond: Boolean,
 			hourRange: Array,
 			minuteRange: Array,
+			secondRange: Array,
 			classe: String,
+			format: String,
+			lang: String,
 			value: {
-				hour: '',
-				minute: '',
+				hour: '0',
+				minute: '0',
+				second : '0',
+				a: '0',
 			}
 		},
 		data: function(){
 			return{
+				withH: this.withHour ? this.withHour : true,
+				withM: this.withMinute ? this.withMinute : true,
+				withS: this.withSecond ? this.withSecond : false,
 				hours_array: [],
 				minutes_array: [],
+				seconds_array: [],
 				hour: '0',
 				minute: '0',
+				second : '0',
+				a: '0',
 				hour_range: this.hourRange ? this.hourRange : ['00-23'],
 				minute_range: this.minuteRange ? this.minuteRange : ['00-59'],
+				second_range: this.secondRange ? this.secondRange : ['00-59'],
 				cl: this.classe ? this.classe : 'form-control col-5',
+				la: this.lang ? this.lang : '',
+				display_lang: {
+					'hour' : 'HH',
+					'min' : 'MM',
+					'sec' : 'SS',
+					'a' : '--',
+				},
 			}
 		},
 		mounted(){
@@ -48,16 +87,37 @@
 		},
 		computed: {
 			initialValue(){
-				this.hour = this.value.hour != "" ? this.value.hour : '0';
-				this.minute = this.value.minute != "" ? this.value.minute : '0';
+				this.hour = this.value && this.value.hour != "" ? this.value.hour : '0';
+				this.minute = this.value && this.value.minute != "" ? this.value.minute : '0';
+				this.second = this.value && this.value.second != "" ? this.value.second : '0';
+				this.a = this.value && this.value.a != "" ? this.value.a : '0';
 			},
 		},
 		created(){
-			this.generateHourMinute(this.hour_range, this.hours_array);
-			this.generateHourMinute(this.minute_range, this.minutes_array);
+			if(this.format == '12' && !this.hourRange){
+				this.hour_range = ['01-12'];
+			}
+			this.generateHourMinuteSecond(this.hour_range, this.hours_array);
+			this.generateHourMinuteSecond(this.minute_range, this.minutes_array);
+			this.generateHourMinuteSecond(this.second_range, this.seconds_array);
+			this.generateLang(this.la);
 		},
 		methods: {
-			generateHourMinute: function(prop, array){
+			generateLang: function(lang){
+				if(this.lang == 'fr'){
+					this.display_lang['hour'] = 'Heures';
+					this.display_lang['min'] = 'Min';
+					this.display_lang['sec'] = 'Sec';
+					this.display_lang['a'] = '--';
+				}
+				if(this.lang == 'en'){
+					this.display_lang['hour'] = 'Hour';
+					this.display_lang['min'] = 'Min';
+					this.display_lang['sec'] = 'Sec';
+					this.display_lang['a'] = '--';
+				}
+			},
+			generateHourMinuteSecond: function(prop, array){
 				let self = this;
 		        let debut = '';
 		        let fin = '';
@@ -97,8 +157,10 @@
 			updateHour:function(){
 				let self = this
 				this.$emit('input', {
-					hour: self.hour != '0' ? self.hour : '',
-					minute: self.minute != '0' ? self.minute : '',
+					hour: self.hour != '0' ? self.hour : '0',
+					minute: self.minute != '0' ? self.minute : '0',
+					second: self.second != '0' ? self.second : '0',
+					a: self.a != '0' ? self.a : '0',
 				});
 			}
 		}
